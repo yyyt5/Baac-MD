6f96d06f9b16072b9999954c2a485167a632fb38
yyyt5 <yrandula0@gmail.com>
2025-03-16 17:35:28 +0530

    Update imagine.js

--- plugins/imagine.js
+++ plugins/imagine.js
@@ -45,7 +45,7 @@ cmd({
       return reply("Please provide a prompt for the image.");
     }
 
-    await reply("> *KHAN-AI CREATING IMAGINE ...🔥*");
+    await reply("> *𝐏𝐎𝐃𝐃𝐀-AI CREATING IMAGINE ...🔥*");
     
     const response = await fetchJson(`https://api.giftedtech.web.id/api/ai/fluximg?apikey=gifted&prompt=${q}`);
     const imageUrl = response.result;
