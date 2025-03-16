31c33c09c84ef71c936d8d77b97687d07f3a9327
yyyt5 <yrandula0@gmail.com>
2025-03-16 17:10:12 +0530

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -37,7 +37,7 @@ cmd({
                 from,
                 {
                     image: { url: image.src },
-                    caption: `*Results For:* ${query}\n\n> *BY KHAN MD ❤️‍🩹*`
+                    caption: `*Results For:* ${query}\n\n> *BY 𝗣𝗢𝗗𝗗𝗔 MD ❤️‍🩹*`
                 },
                 { quoted: mek }
             );
