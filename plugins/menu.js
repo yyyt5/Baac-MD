f4756eb8df9ff4b2da5c1c73d3cb58033ed02632
yyyt5 <yrandula0@gmail.com>
2025-03-20 17:55:25 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -283,15 +283,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/pw7ap2.jpg` },
+                image: { url: `https://i.ibb.co/tPhgkJxx/5079.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'JawadTechX',
+                        newsletterJid: '',
+                        newsletterName: '',
                         serverMessageId: 143
                     }
                 }
