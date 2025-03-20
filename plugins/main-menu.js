449a2867d0c6b67f9b368fe7456a5ef6add9d454
yyyt5 <yrandula0@gmail.com>
2025-03-20 17:34:33 +0530

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -325,8 +325,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'JawadTechX',
+                        newsletterJid: '',
+                        newsletterName: 'RandulTechX',
                         serverMessageId: 143
                     }
                 }
